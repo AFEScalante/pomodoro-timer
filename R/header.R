74d2a4ff7bf6ac67ba3709b553ecba4411ef15a6
2exp3 <agustin.perez.santangelo@gmail.com>
2024-09-13 08:10:44 -0300

    feat: add modal and use clocks in it to determine durations

--- R/header.R
+++ R/header.R
@@ -4,12 +4,55 @@ header_ui <- function(id) {
   tags$header(
     class = "header",
     a(class = "pomodoro-timer", "Pomodoro Timer"),
-    img(class = "editbutton-icon", src = "images/editbutton.svg")
+    actionButton(ns("edit_time"), icon("pencil"))
   )
 }
 
-header_server <- function(id) {
+header_server <- function(id, timer) {
   moduleServer(id, function(input, output, session) {
+    ns <- session$ns
 
+    observeEvent(input$edit_time, {
+      showModal(
+        modalDialog(
+          title = NULL,
+          div(class = "modal-background",
+            div(class = "modal-content",
+              div(
+                class = "clocks",
+                clock_input(ns("pomodoro_duration"), "pomodoro",
+                 value = timer$pomodoro_time, color = timer$mode_colors$pomodoro),
+                clock_input(ns("short_break_duration"), "short break",
+                 value = timer$short_break_time, color = timer$mode_colors$short_break),
+                clock_input(ns("long_break_duration"), "long break",
+                 value = timer$long_break_time, color = timer$mode_colors$long_break)
+              ),
+              actionButton(ns("close_modal"), label = "X", class = "close-btn")
+            )
+          ),
+          easyClose = TRUE,
+          fade = FALSE,
+          footer = NULL
+        )
+      )
+    })
+
+    observeEvent(
+      c(
+        input$pomodoro_duration,
+        input$short_break_duration,
+        input$long_break_duration
+      ), {
+        timer$pomodoro_time <- input$pomodoro_duration
+        timer$short_break_time <- input$short_break_duration
+        timer$long_break_time <- input$long_break_duration
+
+        # Changing durations will stop and reset current timer (including progress).
+        timer$set_mode(timer$current_mode)
+        reset_claim_button()
+      }
+    )
+
+    observeEvent(input$close_modal, removeModal())
   })
 }
